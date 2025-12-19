8941f8a0aa63f8cd279b8e2d28dbfcdbf7b9bc2e
Aditya Sarna <adityasarna@Adityas-MacBook-Air.local>
2025-12-19 12:07:21 +0530

    Change macOS to use --onefile like Windows/Linux

--- hook-pyopenms.py
+++ hook-pyopenms.py
@@ -2,7 +2,19 @@
 # Collects all dynamic libraries that pyopenms depends on
 
 from PyInstaller.utils.hooks import collect_dynamic_libs, collect_data_files
+import os
+from pathlib import Path
 
 # Collect all .dylib files from pyopenms
 datas = collect_data_files('pyopenms', include_py_files=False)
 binaries = collect_dynamic_libs('pyopenms')
+
+# Also explicitly collect all .dylib files from the pyopenms directory
+try:
+    import pyopenms
+    pyopenms_dir = Path(pyopenms.__file__).parent
+    for dylib in pyopenms_dir.glob('*.dylib'):
+        binaries.append((str(dylib), 'pyopenms'))
+except Exception as e:
+    print(f"Warning: Could not collect pyopenms dylibs: {e}")
+
