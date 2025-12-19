ac4c65f78377fbbef0fd61be164f99f9f2c4299b
Aditya Sarna <adityasarna@Adityas-MacBook-Air.local>
2025-12-19 13:11:32 +0530

    Bundle full pyopenms assets in PyInstaller

--- hook-pyopenms.py
+++ hook-pyopenms.py
@@ -1,20 +1,8 @@
 # PyInstaller hook for pyopenms
-# Collects all dynamic libraries that pyopenms depends on
+# Collect the entire package (dylibs, data files, hidden imports) so that the
+# bundled macOS app ships every dependency required by libOpenMS and Qt.
 
-from PyInstaller.utils.hooks import collect_dynamic_libs, collect_data_files
-import os
-from pathlib import Path
+from PyInstaller.utils.hooks import collect_all
 
-# Collect all .dylib files from pyopenms
-datas = collect_data_files('pyopenms', include_py_files=False)
-binaries = collect_dynamic_libs('pyopenms')
-
-# Also explicitly collect all .dylib files from the pyopenms directory
-try:
-    import pyopenms
-    pyopenms_dir = Path(pyopenms.__file__).parent
-    for dylib in pyopenms_dir.glob('*.dylib'):
-        binaries.append((str(dylib), 'pyopenms'))
-except Exception as e:
-    print(f"Warning: Could not collect pyopenms dylibs: {e}")
+datas, binaries, hiddenimports = collect_all('pyopenms')
 
